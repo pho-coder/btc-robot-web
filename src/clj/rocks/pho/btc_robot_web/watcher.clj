c40bde1846bbcdf85deadeae06698fe5bfa3e468
Phoenix Zhang <pho.coderzhang@gmail.com>
2016-07-26 18:19:27 +0800

    fix

--- src/clj/rocks/pho/btc_robot_web/watcher.clj
+++ src/clj/rocks/pho/btc_robot_web/watcher.clj
@@ -1,6 +1,8 @@
 (ns rocks.pho.btc-robot-web.watcher
   (:require [mount.core :as mount]
+            [clojure.tools.logging :as log]
 
+            [rocks.pho.btc-robot-web.config :refer [env]]
             [rocks.pho.btc-robot-web.utils :as utils]))
 
 (mount/defstate history-dir
